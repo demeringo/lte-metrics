b85b168354a3a931d143db950c3abcc44350a07f
DE MERINGO Olivier <olivier.demeringo@cimpa.com>
2024-07-11 09:24:29 +0200

    wip: attempt to generate static site (does not fully work, graphs not displayed)

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import sveltePreprocess from 'svelte-preprocess';
 import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -11,7 +12,21 @@ export default {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: node(),
+    //
+    // Deactivate standard Node app
+    // adapter: node(),
+    //
+    // Configure an adapter for static site generation 
+    // https://kit.svelte.dev/docs/adapter-static
+    adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
     vite: {
       server: {
         fs: {
